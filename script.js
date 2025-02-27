88700f9d07f6b8a5d0399a08387103f280b26c5a
dianna <diannatamfu529@gmial.com>
2025-02-27 21:37:47 +0100

    final view

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let hajust
+let lanAjust
+let refAjust
+let hobby
 
 function ShowLimitError(message) 
 {
@@ -24,8 +27,13 @@ function formAjust(currentIndex)
         }
     }
 
-    else if (currentIndex >= 3 ) {
-        document.querySelector('form').style.height = "540px"   
+    else if (currentIndex >= 3 && currentIndex  < 5 ) 
+    {
+        document.querySelector('form').style.height = "400px"   
+    }
+
+    else if (currentIndex >= 6 ) {
+        document.querySelector('form').style.height = "500px"   
     }
 
     else if(currentIndex == 0)
@@ -159,6 +167,7 @@ function formHandler(id, addId)
                 document.querySelector('form').style.height = `${hajust}px`
 
             }
+
 
             console.log(indexer)
         }
@@ -402,4 +411,168 @@ function syncEdu(e)
 }
 /*To handle education*/
 
+switcher()
+
+
+function removeLanguage(e) {
+    let languageItems = document.querySelectorAll('#language .content > div');
+
+    if (languageItems.length > 1) {
+        e.target.closest('div').remove();
+        reOrderLanguages();
+    } else {
+        ShowLimitError("Trop peu de langues !");
+    }
+}
+
+
+    let languageItems = document.querySelectorAll('#language .content > div');
+    if (languageItems.length > 1) {
+        e.target.closest('div').remove();
+        const languageDeletedEvent = new CustomEvent('language_deleted');
+        document.querySelector("#language").dispatchEvent(languageDeletedEvent);
+    } else {
+        ShowLimitError("Trop peu de langues !");
+    }
+
+
+function languageFormHandler() {
+    let languageItems = document.querySelectorAll("#language .content > div");
+    let indexer = languageItems.length;
+
+    function reOrder() {
+        document.querySelectorAll("#language .content > div").forEach((el, i) => {
+            el.querySelector('.comId').innerText = (i + 1);
+        });
+    }
+
+    function addLanguage() {
+        let languageItems = document.querySelectorAll('#language .content > div');
+    
+        if (languageItems.length < 4) {
+            let newElement = languageItems[0].cloneNode(true);
+            newElement.querySelectorAll('input').forEach(input => input.value = "");
+            newElement.querySelector('.delete-btn').addEventListener("click", removeLanguage);
+            document.querySelector('#language .content').appendChild(newElement);
+            reOrderLanguages();
+        } else {
+            ShowLimitError("Maximum de langues atteint !");
+        }
+    }
+
+ 
+
+    document.querySelector('#language').addEventListener('language_deleted', () => {
+        indexer--;
+        reOrder();
+    });
+
+    document.getElementById("addlanguage").addEventListener('click', addLanguage);
+}
+
+// Hobby Section Functions
+
+function ShowLimitError(message) 
+{
+    document.getElementById('limithobby').innerText = message
+
+    setTimeout(() => {
+        document.getElementById('limithobby').innerText = ""
+    }, 4000);
+}
+
+
+// Remove Hobby Function (Fixed)
+function removeHobby(e) {
+    let hobbyItems = document.querySelectorAll('#hobby .content > div');
+
+    if (hobbyItems.length > 1) {
+        e.target.closest('div').remove();
+        reOrderHobbies();
+    } else {
+        ShowLimitError("Trop peu de loisirs !", "limitHobby");
+    }
+}
+
+
+function hobbyFormHandler() {
+    let hobbyItems = document.querySelectorAll("#hobby .content > div");
+    let indexer = hobbyItems.length;
+
+    function reOrder() {
+        document.querySelectorAll("#hobby .content > div").forEach((el, i) => {
+            el.querySelector('.comId').innerText = (i + 1);
+        });
+    }
+
+    
+    // Add Hobby Function (Fixed)
+function addHobby() {
+    let hobbyItems = document.querySelectorAll('#hobby .content > div');
+
+    if (hobbyItems.length < 3) {
+        let newElement = hobbyItems[0].cloneNode(true);
+        newElement.querySelectorAll('input').forEach(input => input.value = "");
+        newElement.querySelector('.delete-btn').addEventListener("click", removeHobby);
+        document.querySelector('#hobby .content').appendChild(newElement);
+        reOrderHobbies();
+    } else {
+        ShowLimitError("Maximum de loisirs atteint !", "limitHobby");
+    }
+}
+
+    document.querySelector('#hobby').addEventListener('hobby_deleted', () => {
+        indexer--;
+        reOrder();
+    });
+
+    document.getElementById("addhobby").addEventListener('click', addHobby);
+}
+
+
+// REFERENCES SECTION (NEW FEATURE)
+function removeReference(e) {
+    let referenceItems = document.querySelectorAll('#references .content > div');
+
+    if (referenceItems.length > 1) {
+        e.target.closest('div').remove();
+        reOrderReferences();
+    } else {
+        ShowLimitError("Vous devez avoir au moins une référence !", "limitReference");
+    }
+}
+
+// Reorder References
+function reOrderReferences() {
+    document.querySelectorAll("#references .content > div").forEach((el, i) => {
+        el.querySelector('.comId').innerText = (i + 1);
+    });
+}
+
+// Add Reference
+function addReference() {
+    let referenceItems = document.querySelectorAll('#references .content > div');
+
+    if (referenceItems.length < 5) {
+        let newElement = referenceItems[0].cloneNode(true);
+        newElement.querySelectorAll('input').forEach(input => input.value = "");
+        newElement.querySelector('.delete-btn').addEventListener("click", removeReference);
+        document.querySelector('#references .content').appendChild(newElement);
+        reOrderReferences();
+    } else {
+        ShowLimitError("Maximum de références atteint !", "limitReference");
+    }
+}
+
+// Initialize References Section
+function referenceFormHandler() {
+    document.getElementById("addreference").addEventListener('click', addReference);
+    document.querySelectorAll('#references .content > div .delete-btn').forEach(button => {
+        button.addEventListener('click', removeReference);
+    });
+}
+
+languageFormHandler()
+hobbyFormHandler()
+referenceFormHandler() 
 switcher()
